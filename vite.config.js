73388b2bced6993206af60740cb6f622794cfae6
Alexis <alexis.gonzalescs@gmail.com>
2026-07-13 20:36:24 -0500

    fix: update base url for new repository name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SO-EXAMEN-FINAL/'
+  base: '/SISTEMAS-OPERATIVOS_Examen-Final/'
 })
